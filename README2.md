bfd5d5d52992b1c81d11569f731e4d1b7f8b5912
Administrator <admin@example>
2021-09-15 18:41:55 +0800

    add table

---
+++ README2.md
@@ -0,0 +1,2 @@
+# plugin.json  =>  PluginMeta
+# PanelProps 包含 options, data, 其中options是我们在types定义的传给panel的参数；data是DataSource里面的数据源的数据
--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -2,49 +2,25 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
-
+import { stylesFactory, useTheme} from '@grafana/ui';
 interface Props extends PanelProps<SimpleOptions> {}
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+export const SimplePanel: React.FC<Props> = ({ options, data}) => {
   const theme = useTheme();
   const styles = getStyles();
   return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
-
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
-        )}
-        <div>Text option value: {options.text}</div>
-      </div>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>我的表哥</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>我的表哥</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
